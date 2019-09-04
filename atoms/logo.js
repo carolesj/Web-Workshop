6cbf93d437732769a47dfabea5c69db3c7e6a2ac
Gustavo Aguiar <gugahoa@gmail.com>
2019-09-03 21:58:11 -0300

    Add Logo to Navbar

--- atoms/logo.js
+++ atoms/logo.js
@@ -1,5 +1,7 @@
-const Logo = () => {
-  return <div> </div>
+const Logo = (props) => {
+  const { className } = props
+  const santizedClass = className ? className : ""
+  return <img className={santizedClass + " h-full"} src='/static/logo.png' alt='kinase logo'/>
 }
 
 export default Logo;
--- organisms/navbar.js
+++ organisms/navbar.js
@@ -2,8 +2,8 @@ import Logo from '../atoms/logo'
 import SearchBar from '../molecules/searchbar'
 
 export default () => {
-  return <div>
-    <Logo></Logo>
+  return <div className="flex items-center h-16 bg-purple-700">
+    <Logo className="py-1 ml-6"></Logo>
     <SearchBar></SearchBar>
   </div>
 }
