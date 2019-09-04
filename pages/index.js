a85d3ea38438770a1f3528d49972434f77fb59b4
Gustavo Aguiar <gugahoa@gmail.com>
2019-09-03 21:12:27 -0300

    Move searchbar from molecules to organisms

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../organisms/searchbar'
 
 import '../index.css'
 
 const Home = () => (
-  <div></div>
+  <div>
+    <Navbar></Navbar>
+  </div>
 )
 
 export default Home
